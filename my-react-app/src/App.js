b1680b7f5e3dac037be6899c8eb2f81b671b7094
Ifad Zaman <ifadzmn@gmail.com>
2023-04-05 14:46:06 +0600

    styling react components

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,11 @@
-import LayoutComponentTwo from "./components/LayoutComponentTwo";
-import LayoutComponentOne from "./components/LayoutComponent";
+import Logo from "./components/Logo";
+import './assets/css/global.css'
+
 export default function App() {
     return (
-        <div className="app">
-            <LayoutComponentOne />
+        <div className="App">
+            <h1>Learn with Ifad!</h1>
+            <Logo />
         </div>
     );
 }
